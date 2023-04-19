4de5720c50e0b31d81a5726a5577454929e471ac
Salvatore Capano <capano_salvatore@libero.it>
2023-04-19 13:16:28 +0200

    Fix. Controller Registrazione Utente

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,61 +31,19 @@ class RegisteredUserController extends Controller
             'first_name' => ['required', 'string', 'max:64'],
             'last_name' => ['required', 'string', 'max:64'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
-            'user_image' => ['nullable', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'date_of_birth' => ['nullable'],
-            'gender' => ['nullable', 'string', Rule::in(['male', 'female', 'other', 'unspecified'])],
-            'phone_number' => ['nullable', 'numeric'],
-            'address' => ['nullable', 'string'],
         ]);
 
-        // $user = User::create([
-        //     'first_name' => $request->first_name,
-        //     'last_name' => $request->last_name,
-        //     'level' => 'EM',
-        //     'email' => $request->email,
-        //     'password' => Hash::make($request->password),
-        // ]);
-
-        // Creazione nuovo User
         $newUser = new User;
 
-        $newUser->first_name = $request->first_name;
-        $newUser->last_name = $request->last_name;
-        $newUser->level = 'EM';
-        $newUser->email = $request->email;
+        $newUser->first_name = Str::lower($request->first_name);
+        $newUser->last_name = Str::lower($request->last_name);
+        $newUser->email = Str::lower($request->email);
+        $newUser->date_of_birth = $request->date_of_birth;
         $newUser->password = Hash::make($request->password);
 
         $newUser->save();
-
-        // Creazione User Detail
-        $newUserDetail = new UserDetail();
-        
-        $newUserDetail->user_id = $newUser->id;
-        // if ($request->date_of_birth && $request->date_of_birth != 'null') $newUserDetail->date_of_birth = $request->date_of_birth;
-        if ($request->gender) $newUserDetail->gender = $request->gender;
-        if ($request->phone_number) $newUserDetail->phone_number = $request->phone_number;
-        if ($request->address) $newUserDetail->address = $request->address;
-
-        if ($request->date_of_birth && $request->date_of_birth <= new DateTime('2004-12-31')) {
-            $newUserDetail->date_of_birth = $request->date_of_birth;
-        }
-        else {
-            $newUserDetail->date_of_birth = null;
-        }
-
-        // $newUserDetail->gender = $request->gender;
-        // $newUserDetail->phone_number = $request->phone_number;
-        // $newUserDetail->address = $request->address;
-
-        if ($request->file('user_image')) {
-            $image = $request->file('user_image');
-            $imageName = time() . '.' . $image->extension();
-            $newUserDetail->user_image = $imageName;
-            $image->storeAs('public/user_images', $imageName);
-        }
-
-        $newUserDetail->save();
 
         event(new Registered($newUser));
 
@@ -93,12 +51,4 @@ class RegisteredUserController extends Controller
 
         return response()->noContent();
     }
-
-    // public function messages()
-// {
-    // return [
-    //     'price.required' => 'You must have a price.',
-    //     'price.numeric' => 'You have invalid characters in the price field'
-    // ];
-// }
 }
