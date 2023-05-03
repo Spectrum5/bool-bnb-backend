c2878c53732364c11c7ccf010106f52e33f526e5
GiadaMarzapane <giada.ortesta@gmail.com>
2023-05-03 11:14:37 +0200

    Update seeder

--- database/seeders/ApartmentTableSeeder.php
+++ database/seeders/ApartmentTableSeeder.php
@@ -54,27 +54,27 @@ class ApartmentTableSeeder extends Seeder
                 $newApartment->bathrooms_number = 3;
                 $newApartment->beds_number = random_int(5, 6);
             }
-            elseif ($newApartment->price > 300 && $newApartment->price <= 500) {
+            elseif ($newApartment->price > 500 && $newApartment->price <= 700) {
                 $newApartment->rooms_number = 4;
                 $newApartment->bathrooms_number = 4;
                 $newApartment->beds_number = random_int(7, 8);
             }
-            elseif ($newApartment->price > 500 && $newApartment->price <= 700) {
+            elseif ($newApartment->price > 700 && $newApartment->price <= 900) {
                 $newApartment->rooms_number = 5;
                 $newApartment->bathrooms_number = 5;
                 $newApartment->beds_number = random_int(9, 10);
             }
-            elseif ($newApartment->price > 700 && $newApartment->price <= 900) {
+            elseif ($newApartment->price > 900 && $newApartment->price <= 1100) {
                 $newApartment->rooms_number = 6;
                 $newApartment->bathrooms_number = 6;
                 $newApartment->beds_number = random_int(11, 12);
             }
-            elseif ($newApartment->price > 900 && $newApartment->price <= 1100) {
+            elseif ($newApartment->price > 1100 && $newApartment->price <= 1300) {
                 $newApartment->rooms_number = 7;
                 $newApartment->bathrooms_number = 7;
                 $newApartment->beds_number = random_int(13, 14);
             }
-            elseif ($newApartment->price > 1100 && $newApartment->price <= 1500) {
+            elseif ($newApartment->price > 1300 && $newApartment->price <= 1500) {
                 $newApartment->rooms_number = 8;
                 $newApartment->bathrooms_number = 8;
                 $newApartment->beds_number = random_int(15, 16);
@@ -82,7 +82,11 @@ class ApartmentTableSeeder extends Seeder
 
             $newApartment->description = $singleApartment['description'];
             $newApartment->size = $faker->numberBetween(50, 500);
-            $newApartment->visibility = $faker->boolean();
+            
+            if (rand(1,100) > 90) {
+                $newApartment->visibility = 0;
+            }
+            else $newApartment->visibility = 1;
 
             $newApartment->save();
         }
